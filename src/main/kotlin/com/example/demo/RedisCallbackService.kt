53b4a7a000ef40f7486cedde73115d5e983070da
perfectacle <perfectacle@gmail.com>
2019-12-08 22:57:34 +0900

    RedisCallback 인터페이스를 사용하는 코드 작성

---
+++ src/main/kotlin/com/example/demo/RedisCallbackService.kt
@@ -0,0 +1,31 @@
+package com.example.demo
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Service
+
+
+// RedisCallback 인터페이스를 사용하면 레디스와 다이렉트로 통신할 수 있다고 한다.
+// 좀 더 덜 추상화 된 low-layer를 다룰 때 쓰면 될 듯... (성능 최적화 등등)
+@Service
+class RedisCallbackService(
+        private val redisTemplate: RedisTemplate<String, String>
+) {
+    fun set(key: String, value: String) {
+        redisTemplate.execute {
+            it[key.toByteArray()] = value.toByteArray()
+        }
+    }
+
+    fun get(key: String): String? {
+        return redisTemplate.execute {
+            val bytes = it[key.toByteArray()] ?: byteArrayOf()
+            String(bytes)
+        }
+    }
+
+    fun delete(key: String) {
+        redisTemplate.execute {
+            it.del(key.toByteArray())
+        }
+    }
+}
---
+++ src/test/kotlin/com/example/demo/RedisCallbackServiceTest.kt
@@ -0,0 +1,43 @@
+package com.example.demo
+
+import org.assertj.core.api.Assertions
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+private const val KEY = "callback-key"
+private const val VALUE = "callback-value"
+
+@SpringBootTest
+class RedisCallbackServiceTest {
+    @Autowired
+    private lateinit var redisCallbackService: RedisCallbackService
+
+    @BeforeEach
+    fun setup() {
+        redisCallbackService.delete(KEY)
+    }
+
+    @Test
+    fun `레디스에 캐시가 없으면 null을 반환한다`() {
+        // When
+        val actual = redisCallbackService.get(KEY)
+
+        // Then
+        Assertions.assertThat(actual).isNull()
+    }
+
+    @Test
+    fun `레디스에 캐시가 있으면 캐시를 반환한다`() {
+        // Given
+        redisCallbackService.set(KEY, VALUE)
+
+        // When
+        val actual = redisCallbackService.get(KEY)
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(VALUE)
+    }
+}
